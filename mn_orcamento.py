6642a477db86665151f170f365f4c805079d9058
kaiosp <kaio.souza.pereira12@gmail.com>
2024-09-21 23:46:43 -0300

    KAIO2

--- mn_orcamento.py
+++ mn_orcamento.py
@@ -1,14 +1,17 @@
 import flet as ft
 from orc_paredes import mostrar_parede  # Importa a função mostrar_parede
 from orc_eletrica import mostrar_eletrica  # Importa a função mostrar_eletrica
-
+from orc_laje import mostrar_laje 
 def orcamento(page):
     page.controls.clear()
     page.add(ft.Text("Tela de Orçamento", size=24))
 
     btn_parede = ft.ElevatedButton(text="Parede", on_click=lambda e: mostrar_parede(page), width=200)
     btn_eletrica = ft.ElevatedButton(text="Elétrica", on_click=lambda e: mostrar_eletrica(page), width=200)
-    page.add(btn_eletrica)
-    page.add(btn_parede)
+    btn_laje = ft.ElevatedButton(text="Laje", on_click=lambda e: mostrar_laje(page), width=200)
+    
+   
+    page.add(btn_eletrica,btn_parede,btn_laje)
+    
     
     page.update()
