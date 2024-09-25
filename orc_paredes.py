a87d6255da2e335060dd6940f48b81f3f0bb5cb8
kaiosp <kaio.souza.pereira12@gmail.com>
2024-09-24 23:57:03 -0300

    KAIO

--- orc_paredes.py
+++ orc_paredes.py
@@ -5,7 +5,7 @@ def mostrar_parede(page):
     page.add(ft.Text("Tela de Parede", size=24))
 
     altura_input = ft.TextField(label="Altura (m)", keyboard_type=ft.KeyboardType.NUMBER)
-    largura_input = ft.TextField(label="Largura (m)", keyboard_type=ft.KeyboardType.NUMBER)
+    comprimento_input = ft.TextField(label="comprimento (m)", keyboard_type=ft.KeyboardType.NUMBER)
     valor_m2_input = ft.TextField(label="Valor por m²", keyboard_type=ft.KeyboardType.NUMBER)
 
     resultado_text = ft.Text("Custo Total: R$ 0.00", size=18)
@@ -13,9 +13,14 @@ def mostrar_parede(page):
     def calcular(e):
         try:
             altura = float(altura_input.value)
-            largura = float(largura_input.value)
+
+            comprimento = float(comprimento_input.value)
+
             valor_m2 = float(valor_m2_input.value)
-            custo_total = altura * largura * valor_m2
+
+            custo_total = altura * comprimento * valor_m2
+
+
             resultado_text.value = f"Custo Total: R$ {custo_total:.2f}"
             page.update()
         except ValueError:
@@ -27,7 +32,7 @@ def mostrar_parede(page):
                                         on_click=lambda e: voltar(page), 
                                         width=200, bgcolor=ft.colors.RED, color=ft.colors.WHITE)
 
-    page.add(altura_input, largura_input, valor_m2_input, calcular_button, resultado_text, voltar_button)
+    page.add(altura_input, comprimento_input, valor_m2_input, calcular_button, resultado_text, voltar_button)
     page.update()
 
 def voltar(page):
