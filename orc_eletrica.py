4200e19c275819551beadbe1028b3009048ead5c
Lucasgithubbb <lucaslopesriibeiro@gmail.com>
2024-09-30 23:29:56 -0300

    Elétrica

--- orc_eletrica.py
+++ orc_eletrica.py
@@ -5,18 +5,18 @@ def mostrar_eletrica(page):
     page.controls.clear()
     page.add(ft.Text("Tela de elétrica", size=24))
 
-    altura_input = ft.TextField(label="Altura (m)", **stl.input_style)
-    largura_input = ft.TextField(label="Largura (m)", **stl.input_style)
-    valor_m2_input = ft.TextField(label="Valor por m²", **stl.input_style)
+    ponto_input = ft.TextField(label="pontos elétricos", **stl.input_style)
+
+    valorPonto_input = ft.TextField(label= "Valor cobrado por ponto", **stl.input_style)
 
     resultado_text = ft.Text("Custo Total: R$ 0.00", size=18)
 
     def calcular(e):
         try:
-            altura = float(altura_input.value)
-            largura = float(largura_input.value)
-            valor_m2 = float(valor_m2_input.value)
-            custo_total = altura * largura * valor_m2
+            ponto = float(ponto_input.value)
+   
+            valor_ponto = float(valorPonto_input.value)
+            custo_total = ponto *   valor_ponto
             resultado_text.value = f"Custo Total: R$ {custo_total:.2f}"
             page.update()
         except ValueError:
@@ -26,7 +26,7 @@ def mostrar_eletrica(page):
     calcular_button = ft.ElevatedButton(text="Calcular", on_click=calcular, **stl.button_style)
     voltar_button = ft.ElevatedButton(text="Voltar", on_click=lambda e: voltar(page), **stl.button_style_voltar)
 
-    page.add(altura_input, largura_input, valor_m2_input, calcular_button, resultado_text, voltar_button)
+    page.add(ponto_input, valorPonto_input, calcular_button, resultado_text, voltar_button)
     page.update()
 
 def voltar(page):
