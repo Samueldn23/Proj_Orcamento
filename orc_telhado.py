aa959eb5d7bc2ac20e3df932d7d15e0b120b74cf
Paulo Matos <123278603+Paulomatos104@users.noreply.github.com>
2024-10-11 18:39:03 -0300

    Primeira versão haverá mudanças

--- orc_telhado.py
+++ orc_telhado.py
@@ -4,15 +4,15 @@ import locale
 # Define a localidade para pt_BR
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-def mostrar_contrapiso(page):
+def mostrar_telhado(page):
     page.controls.clear()
 
-    page.add(ft.Text("Contrapiso", size=24))
+    page.add(ft.Text("Telhado", size=24))
 
     comprimento_input = ft.TextField(label="Comprimento (m)", keyboard_type=ft.KeyboardType.NUMBER)
     largura_input = ft.TextField(label="Largura (m)", keyboard_type=ft.KeyboardType.NUMBER)
     valor_input = ft.TextField(label="Valor do Metro (R$)", keyboard_type=ft.KeyboardType.NUMBER)
-    espessura_input = ft.TextField(label="Espessura (Cm)", keyboard_type=ft.KeyboardType.NUMBER, visible=False)
+    
     resultado_text = ft.Text("Custo Total: R$ 0.00", size=18)
 
     # Função para calcular o custo
@@ -22,25 +22,13 @@ def mostrar_contrapiso(page):
             largura = float(largura_input.value)
             ValorM2 = float(valor_input.value)
 
-            if switch.value:  # switch para metros cúbicos
-                espessura = float(espessura_input.value)
-                calculo_m3 = largura * comprimento * (espessura / 100)  # Converter espessura para metros
+                calculo_m2 = largura * comprimento * (espessura / 100) 
                 custo_total = calculo_m3 * ValorM2
-            else:  # metros quadrados
-                calculo_m2 = largura * comprimento
-                custo_total = calculo_m2 * ValorM2
 
             resultado_text.value = f"Custo Total: {locale.currency(custo_total, grouping=True)}"
         except ValueError:
             resultado_text.value = "Por favor, insira valores válidos."
         page.update()
-
-    # Função para atualizar visibilidade do campo de espessura
-    def atualizar(e):
-        espessura_input.visible = switch.value
-        page.update()
-
-    switch = ft.Switch(label="Metro Quadrado para Metro Cúbico", on_change=atualizar, value=False)
 
     calcular_button = ft.ElevatedButton(text="Calcular", on_click=calcular, width=200)
     voltar_button = ft.ElevatedButton(text="Voltar", on_click=lambda e: voltar(page), width=200, bgcolor=ft.colors.RED, color=ft.colors.WHITE)
