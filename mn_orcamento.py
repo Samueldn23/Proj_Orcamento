05235c09532669b2c289b3310c802bd326482996
Paulo matos <paulo.dematos23@gmail.com>
2024-10-18 10:28:21 -0300

    Atualização e add style

--- mn_orcamento.py
+++ mn_orcamento.py
@@ -4,28 +4,37 @@ from orc_eletrica import mostrar_eletrica  # Importa a função mostrar_eletrica
 from orc_laje import mostrar_laje
 from exemplos import exemplo
 from orc_laje import mostrar_laje
-from orc_contrapiso import mostrar_contrapiso 
+from orc_contrapiso import mostrar_contrapiso
 from orc_telhado import mostrar_telhado
+
 
 def orcamento(page):
     page.controls.clear()
     page.add(ft.Text("Tela de Orçamento", size=24))
 
-    btn_parede = ft.ElevatedButton(text="Parede", on_click=lambda e: mostrar_parede(page), width=200)
-    btn_eletrica = ft.ElevatedButton(text="Elétrica", on_click=lambda e: mostrar_eletrica(page), width=200)
-    btn_laje = ft.ElevatedButton(text="Laje", on_click=lambda e: mostrar_laje(page), width=200)
-    btn_contrapiso = ft.ElevatedButton(text="Contrapiso", on_click=lambda e: mostrar_contrapiso(page), width=200)
-    btn_telhado = ft.ElevatedButton(text="Telhado", on_click=lambda e: mostrar_contrapiso(page), width=200)
-    btn_exemplo = ft.ElevatedButton(text="Exemplo", on_click=lambda e: exemplo(page), width=200)
+    btn_parede = ft.ElevatedButton(
+        text="Parede",
+        on_click=lambda e: mostrar_parede(page),
+        width=200,
+    )
+    btn_eletrica = ft.ElevatedButton(
+        text="Elétrica", on_click=lambda e: mostrar_eletrica(page), width=200
+    )
+    btn_laje = ft.ElevatedButton(
+        text="Laje", on_click=lambda e: mostrar_laje(page), width=200
+    )
+    btn_contrapiso = ft.ElevatedButton(
+        text="Contrapiso", on_click=lambda e: mostrar_contrapiso(page), width=200
+    )
+    btn_telhado = ft.ElevatedButton(
+        text="Telhado", on_click=lambda e: mostrar_telhado(page), width=200
+    )
+    btn_exemplo = ft.ElevatedButton(
+        text="Exemplo", on_click=lambda e: exemplo(page), width=200
+    )
 
-    
     page.add(
-        btn_exemplo,
-        btn_eletrica,
-        btn_parede,
-        btn_laje,
-        btn_contrapiso,
-        btn_telhado)
-    
-    
+        btn_exemplo, btn_eletrica, btn_parede, btn_laje, btn_contrapiso, btn_telhado
+    )
+
     page.update()
